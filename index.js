9ce8bb704b6c75bbad6c0204169dd73301ca90d9
santhoshkrishnaks <santhoshkrishna771@gmail.com>
2024-07-25 17:09:17 +0530

    Completion without responsive

---
+++ index.js
@@ -0,0 +1,14 @@
+
+    ScrollReveal({
+            reset: false,
+            distance: "180px",
+            duration: 2000,
+            delay: 200,
+        });
+        ScrollReveal().reveal(".header-area",{delay:340,origin:"top"});
+        ScrollReveal().reveal(".Profile",{delay:640,origin:"left"});
+        ScrollReveal().reveal(".profile-text",{delay:640,origin:"right"});
+        ScrollReveal().reveal(".col-twelve",{delay:640,origin:"right"});
+        ScrollReveal().reveal(".project-container",{delay:640,origin:"left"});
+        ScrollReveal().reveal(".section .Skills",{delay:640,origin:"right"});
+        ScrollReveal().reveal(".contact .container",{delay:640,origin:"left"});
